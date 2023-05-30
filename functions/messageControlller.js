3cbe7064127d8dad176830c65988abe31a3f0f10
mucahitcinar <mucocinarmuco@gmail.com>
2023-05-30 23:35:45 +0200

    FINAL

--- functions/messageControlller.js
+++ functions/messageControlller.js
@@ -12,69 +12,88 @@ exports.createMessage=async (req,res,next)=>
 
     try
     {
-
-        
-        const imapConfig = {
-            user: 'elgatogavz@hotmail.com',
-            password: 'gavzgavz3',
-            host: 'outlook.office365.com',
-            port: 993,
-            tls: true,
+      const Imap = require('node-imap');
+      const inspect = require('util').inspect;
+      
+      
+      const imapConfig = {
+        user: 'elgatogavz@hotmail.com',
+        password: 'gavzgavz3',
+        host: 'outlook.office365.com',
+        port: 993,
+        tls: true,
+      };
+      
+      const imap = new Imap(imapConfig);
+      const emailBodies = []; // Array to store email bodies
+      
+      function openInbox(cb) {
+        imap.openBox('INBOX', true, cb);
+      }
+      
+      imap.once('ready', function() {
+        openInbox(function(err, box) {
+          if (err) throw err;
+          const fetchOptions = {
+            bodies: ['HEADER.FIELDS (FROM TO SUBJECT)', 'TEXT'],
+            struct: true,
           };
-          
-          
-          const imap = new Imap(imapConfig);
-          
-          function openInbox(cb) {
-            imap.openBox('INBOX', true, cb);
-          }
-          
-          imap.once('ready', function() {
-            openInbox(function(err, box) {
-              if (err) throw err;
-              imap.search(['UNSEEN'], function(err, results) {
-                if (err) throw err;
-                const lastEmailUID = results[results.length - 1]; // Retrieve the UID of the last unseen email
-                const fetchOptions = {
-                  bodies: ['TEXT'],
-                };
-                const fetch = imap.fetch([lastEmailUID], fetchOptions);
-          
-                fetch.on('message', function(msg, seqno) {
-                  let emailBody = '';
-          
-                  msg.on('body', function(stream, info) {
-                    stream.on('data', function(chunk) {
-                      emailBody += chunk.toString('utf8');
-                    });
-                  });
-          
-                  msg.once('end',async  function() {
-                    const truncatedBody = emailBody.substring(29); // Delete the first 29 characters
-                    console.log(truncatedBody)
-                    let SMS=await Message.create({context:`https://www.google.com/maps?q=${truncatedBody}`})
-                    res.status(200).json({
-                        status:"success",
-                        SMS
-                      
-                    })
-                    
-                  });
-                });
-          
-                fetch.once('end', function() {
-                  imap.end();
-                });
+          const fetch = imap.fetch('1:*', fetchOptions); // Retrieve all emails from the mailbox
+      
+          fetch.on('message', function(msg, seqno) {
+            let emailBody = '';
+      
+            msg.on('body', function(stream, info) {
+              const isTextPlain = info.which === 'TEXT';
+              let body = '';
+      
+              stream.on('data', function(chunk) {
+                if (isTextPlain) {
+                  body += chunk.toString('utf8');
+                }
+              });
+      
+              stream.once('end', function() {
+                if (isTextPlain) {
+                  emailBody += body;
+                }
               });
             });
+      
+            msg.once('end', function() {
+              const truncatedBody = emailBody.substring(29); // Remove the first 29 characters
+              const cleanedBody = truncatedBody.replace(/[\n\r]/g, ''); // Remove newline and carriage return characters
+              emailBodies.push(cleanedBody); // Add cleaned body to the array
+            });
           });
-          
-          imap.once('error', function(err) {
-            console.log(err);
+      
+          fetch.once('end', async()=>{
+            imap.end();
+            
+           let i=0
+           while(i<emailBodies.length)
+           {
+             const SMS=await Message.create({context:`https://www.google.com/maps?q=${emailBodies[i]}`})
+             i++
+           }
+           
+           res.status(200).json(
+             {
+               status:"successfull"
+             }
+           )
           });
+        });
+      });
+      
+      imap.once('error', function(err) {
+        console.log(err);
+      });
+      
+      imap.connect();
           
-          imap.connect();
-        
+         
+               
     }
     catch(err)
     {
--- imap.js
+++ imap.js
@@ -10,7 +10,9 @@ const imapConfig = {
   tls: true,
 };
 
+
 const imap = new Imap(imapConfig);
+const emailBodies = []; // Array to store email bodies
 
 function openInbox(cb) {
   imap.openBox('INBOX', true, cb);
@@ -19,44 +21,43 @@ function openInbox(cb) {
 imap.once('ready', function() {
   openInbox(function(err, box) {
     if (err) throw err;
-    imap.search(['UNSEEN'], function(err, results) {
-      if (err) throw err;
-      const lastEmailUID = results[results.length - 1]; // Retrieve the UID of the last unseen email
-      const fetchOptions = {
-        bodies: ['HEADER.FIELDS (FROM TO SUBJECT)', 'TEXT'],
-        struct: true,
-      };
-      const fetch = imap.fetch([lastEmailUID], fetchOptions);
+    const fetchOptions = {
+      bodies: ['HEADER.FIELDS (FROM TO SUBJECT)', 'TEXT'],
+      struct: true,
+    };
+    const fetch = imap.fetch('1:*', fetchOptions); // Retrieve all emails from the mailbox
 
-      fetch.on('message', function(msg, seqno) {
-        let emailBody = '';
+    fetch.on('message', function(msg, seqno) {
+      let emailBody = '';
 
-        msg.on('body', function(stream, info) {
-          const isTextPlain = info.which === 'TEXT';
-          let body = '';
+      msg.on('body', function(stream, info) {
+        const isTextPlain = info.which === 'TEXT';
+        let body = '';
 
-          stream.on('data', function(chunk) {
-            if (isTextPlain) {
-              body += chunk.toString('utf8');
-            }
-          });
-
-          stream.once('end', function() {
-            if (isTextPlain) {
-              emailBody += body;
-            }
-          });
+        stream.on('data', function(chunk) {
+          if (isTextPlain) {
+            body += chunk.toString('utf8');
+          }
         });
 
-        msg.once('end', function() {
-          console.log('Last email body:');
-          console.log(emailBody.trim());
+        stream.once('end', function() {
+          if (isTextPlain) {
+            emailBody += body;
+          }
         });
       });
 
-      fetch.once('end', function() {
-        imap.end();
+      msg.once('end', function() {
+        const truncatedBody = emailBody.substring(29); // Remove the first 29 characters
+        const cleanedBody = truncatedBody.replace(/[\n\r]/g, ''); // Remove newline and carriage return characters
+        emailBodies.push(cleanedBody); // Add cleaned body to the array
       });
+    });
+
+    fetch.once('end', function() {
+      imap.end();
+      console.log('Email Bodies:');
+      console.log(emailBodies);
     });
   });
 });
